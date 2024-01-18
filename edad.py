6464ba13f3ea1861a4e0c629de24f50f04d3b07f
Andres Arroyo <aearroyogo@cesde.net>
2024-01-18 06:38:33 -0500

    Se agregaron y modificaron archivos

---
+++ edad.py
@@ -0,0 +1,6 @@
+nombre = "Carlos"
+Edad = 32
+if Edad >= 18:
+     print ("Eres mayor edad")
+else:
+    print("Eres menor de edad")
---
+++ par.py
@@ -0,0 +1,4 @@
+numero = 120
+if numero %2 == 0:
+   print ("Es par")
+
